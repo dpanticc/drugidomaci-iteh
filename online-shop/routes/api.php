9867901584cd8cd5551f081595a6bb64c5695219
Dusan Pantic <dpanticc@gmail.com>
2023-04-15 13:45:28 +0200

    First push, making of models Proizvod and Vrsta (artisan migration) connecting then, altering table name...

--- online-shop/routes/api.php
+++ online-shop/routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('vrsta', 'App\Http\Controllers\VrstaController@getAll');
+Route::get('proizvod', 'App\Http\Controllers\ProizvodController@getAll');
+Route::get('proizvod/{id}', 'App\Http\Controllers\ProizvodController@getById');
+Route::post('proizvod', 'App\Http\Controllers\ProizvodController@save');
+Route::delete('proizvod/{id}', 'App\Http\Controllers\ProizvodController@delete');
+
