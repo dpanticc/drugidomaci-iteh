8ad428c57c95faacf7990687fbbe78ebadc2d994
Dusan Pantic <dpanticc@gmail.com>
2023-04-18 18:10:43 +0200

    slucajno izbrisane rute i funkcije za proizvod controller...

--- online-shop/app/Http/Controllers/ProizvodController.php
+++ online-shop/app/Http/Controllers/ProizvodController.php
@@ -26,5 +26,38 @@ class ProizvodController extends Controller
         ]);
     }
 
+    public function store(Request $request){
 
+        $validator = Validator::make($request->all(), [
+            'naziv'=>'required|min:2',
+            'opis'=>'required|min:2',
+            'slika'=>'required|min:2',
+            'cena'=>'required',
+            'energetski_razred'=>'required',
+            'boja'=>'required|min:2',
+            'materijal'=>'required|min:2',
+            'dimenzije'=>'required|min:2',
+            'vrsta_id'=>'required',
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["message"=>"Sva polja su obavezna"],400);
+        }
+        $proizvod= Proizvod::create($request->all());
+        return response()->json($proizvod, 201);
+    }
+    
+    public function delete($id) {
+        $proizvod = Proizvod::find($id);
+    
+        if (is_null($proizvod)) {
+            return response()->json(['message' => 'Proizvod not found'], 404);
+        }
+    
+        $proizvod->delete();
+    
+        return response()->json(['message' => 'Proizvod deleted successfully']);
+    }
+    
 }
